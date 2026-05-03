aecb98278b23c41a0d2bccc3c76173ed9757a5de
Andrea Solinas <andrea.solinas.99@gmail.com>
2026-05-03 19:55:30 +0200

    Refactor models

--- ingestion/models.py
+++ ingestion/models.py
@@ -4,25 +4,34 @@ from django.db import models
 # Create your models here.
 
 class Campaign(models.Model):
-    campaign_id = models.IntegerField()
+    campaign_id = models.IntegerField(unique=True)
     name = models.CharField(max_length=255)
-    active = models.BooleanField()
+    active = models.BooleanField(default=True)
     site_id = models.IntegerField()
-    schedule_type = models.CharField(max_length=255)
+    schedule_type = models.CharField(max_length=100)
     mailing_list_id = models.IntegerField()
+    fetched_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return f"{self.__class__.__name__}(campaign_id={self.campaign_id}, name={self.name}, active={self.active}, site_id={self.site_id}, schedule_type={self.schedule_type}, mailing_list_id={self.mailing_list_id})"
 
 
 class Analytics(models.Model):
-    campaign = models.ForeignKey(Campaign, on_delete=models.SET_NULL, null=True)
-    datetime = models.DateTimeField()
+    campaign = models.ForeignKey(Campaign, on_delete=models.DO_NOTHING, null=True)
+    date = models.DateField()
     sent = models.BigIntegerField()
     opened = models.BigIntegerField()
     clicked = models.BigIntegerField()
     subscribed = models.BigIntegerField()
     unsubscribed = models.BigIntegerField()
+    fetched_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ('-date',)
+        unique_together = ('campaign', 'date')
+        indexes = [
+            models.Index(fields=['campaign', 'date']),
+        ]
 
     def __str__(self):
-        return f"{self.__class__.__name__}(campaing={self.campaign}, datetime={self.datetime}, sent={self.sent}, opened={self.opened}, clicked={self.clicked}, subscribed={self.subscribed}, unsubscribed={self.unsubscribed})"
+        return f"{self.__class__.__name__}(campaing={self.campaign}, date={self.date}, sent={self.sent}, opened={self.opened}, clicked={self.clicked}, subscribed={self.subscribed}, unsubscribed={self.unsubscribed})"
