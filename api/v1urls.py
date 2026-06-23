8c367be47067af74e28adefd31b1581cec83ac34
Andrea Solinas <andrea.solinas.99@gmail.com>
2026-06-23 15:46:18 +0200

    Refactor: Analytics and Campaing to DRF
    update requirements

--- api/v1urls.py
+++ api/v1urls.py
@@ -8,6 +8,9 @@ from rest_framework.authtoken import views as auth_views
 
 router = DefaultRouter(trailing_slash=False)
 router.register('image', image.views.ImageViewSet, basename='image')
+
+router.register('stats', views.AnalyticsViewSet, basename='analytics')
+router.register('campaign', views.CampaignViewSet, basename='campaign')
 router.register('shortener', url_shorter.views.UrlShorterViewSet, basename='shortener')
 
 urlpatterns = router.urls
@@ -15,17 +18,6 @@ urlpatterns = router.urls
 DATE_PATTERN = r"\d{8}"
 
 urlpatterns += [
-    # re_path(
-    #     rf"stats(/(?P<from_date>{DATE_PATTERN}))?(/(?P<to_date>{DATE_PATTERN}))?/(all|campaign/(?P<campaign_id>[0-9]+))$",
-    #     views.get_stats
-    # ),
-    # re_path(
-    #     rf"stats/aggregate/(?P<from_date>{DATE_PATTERN})(/(?P<to_date>{DATE_PATTERN}))?(/(?P<campaign_id>[0-9]+))?$",
-    #     views.get_aggregated_stats,
-    # ),
-    # path(f"stats/users", views.get_users, name="users"),
-
-
     path('token', auth_views.obtain_auth_token)
 
 ]
